17329ec6c358b499904261beacdaaa4c9022b862
Owner <elihavkmarleker9@gmail.com>
2026-01-05 19:01:10 +0200

    the class Enemy

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import random
 class Enemy(character):
+    def __init__(self, center_x, center_y, speed, change_x, change_y, time_to_change_direction):
+        super().__init__(center_x, center_y, speed, change_x, change_y)
+        self.time_to_change_direction = time_to_change_direction
+    def pick_new_direction(self):
+        directions = ["up" , "down" , "left" , "right"]
+        direction = random.choice(directions)
